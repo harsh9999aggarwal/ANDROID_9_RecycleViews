25fdd9187133312fd034da24d42c2d73d5f163fa
HARSH AGGARWAL <44154669+harsh9999aggarwal@users.noreply.github.com>
2020-09-07 21:36:14 +0530

    Add files via upload

---
+++ FruitAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.listviews
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.list_item_fruit.view.*
+
+class FruitAdapter (
+    val fruits : ArrayList<Fruit>
+) : RecyclerView.Adapter<FruitAdapter.FruitViewHolder>(){
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FruitViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.list_item_fruit,parent,false
+        )
+        return FruitViewHolder((itemView))
+    }
+
+    override fun getItemCount(): Int = fruits.size
+
+    override fun onBindViewHolder(holder: FruitViewHolder, position: Int) {
+        holder.itemView.tvFruitName.text = fruits[position].name
+        holder.itemView.tvOrigin.text = fruits[position].origin
+        holder.itemView.tvQuantity.text = fruits[position].quantity.toString()
+    }
+
+    class FruitViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+}
